d2af74ded6f6c8d6774f1d93f7ff4f1c39541f8a
MeerkatBoss <ivan.solodovnikov@outlook.com>
2024-09-03 23:42:15 +0300

    feat: implement linked list

---
+++ src/Common/List.h
@@ -0,0 +1,100 @@
+/**
+ * @file List.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.edu)
+ *
+ * @brief
+ *
+ * @version 0.1
+ * @date 2024-09-03
+ *
+ * @copyright Copyright MeerkatBoss (c) 2024
+ */
+#ifndef __COMMON_LIST_H
+#define __COMMON_LIST_H
+
+#include <assert.h>
+
+struct LinkedListNode {
+  struct LinkedListNode* next;
+  struct LinkedListNode* prev;
+};
+
+/**
+ * @brief Initiliaze empty linked list
+ */
+static inline void list_init(struct LinkedListNode* node) {
+  node->next = node;
+  node->prev = node;
+}
+
+/**
+ * @brief Check if linked list is empty.
+ * @return 1 if list is empty, 0 otherwise
+ */
+static inline int list_is_empty(struct LinkedListNode* node) {
+  if (node->next == node) {
+    assert(node->prev == node);
+    return 1;
+  }
+  return 0;
+}
+
+/**
+ * @brief Delete node from linked list, turning the node into empty list
+ * @return Deleted node
+ */
+static inline struct LinkedListNode* list_take_node(struct LinkedListNode* node) {
+  if (list_is_empty(node)) {
+    return node;
+  }
+  struct LinkedListNode* prev = node->prev;
+  struct LinkedListNode* next = node->next;
+  prev->next = next;
+  next->prev = prev;
+  list_init(node);
+  return node;
+}
+
+/**
+ * @brief Insert new node after given node
+ *
+ * @warning New node should not already be in list
+ */
+static inline void list_insert_node(
+    struct LinkedListNode* after,
+    struct LinkedListNode* node
+) {
+  assert(after != node);
+  struct LinkedListNode* before = after->next;
+  assert(node != before);
+  assert(before->prev == after);
+
+  after->next = node;
+  before->prev = node;
+  node->prev = after;
+  node->next = before;
+}
+
+
+/**
+ * @brief Iterate over linked list
+ *
+ * @param name Name of iteration variable.
+ * @param list Non-pointer root of linked list.
+ *             Root is assumed to not contain actual data.
+ */
+#define LIST_FOREACH(name, list)\
+  for (\
+      struct LinkedListNode* node = (list).next;\
+      node != &list;\
+      node = node->next\
+  )
+
+#define LIST_CONST_FOREACH(name, list)\
+  for (\
+      const struct LinkedListNode* node = (list).next;\
+      node != &list;\
+      node = node->next\
+  )
+
+#endif /* List.h */
--- src/Main.c
+++ src/Main.c
@@ -1,7 +1,42 @@
+#include <assert.h>
 #include <stdio.h>
+
+#include "Common/List.h"
+
+struct IntList {
+  struct LinkedListNode as_list;
+  int value;
+};
+
+static void int_list_print(const struct LinkedListNode* list) {
+  LIST_CONST_FOREACH(node, *list) {
+    const struct IntList* int_node = (const struct IntList*) node;
+    printf("%d ", int_node->value);
+  }
+  puts("");
+}
 
 int main()
 {
-  puts("Hello, corgi!");
+  struct LinkedListNode int_list;
+  list_init(&int_list);
+
+  struct IntList nodes[5];
+  for (size_t i = 0; i < 5; ++i) {
+    nodes[i].value = i;
+    list_insert_node(int_list.prev, &nodes[i].as_list);
+  }
+  int_list_print(&int_list);
+
+  struct IntList* two = (struct IntList*) int_list.next->next->next;
+  assert(two->value == 2);
+
+  list_take_node(&two->as_list);
+  int_list_print(&int_list);
+
+  list_insert_node(&int_list, &two->as_list);
+  int_list_print(&int_list);
+
+
   return 0;
 }
