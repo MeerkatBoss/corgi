fec459ff269c78a7ba4b9b4b30dd3b9120707230
MeerkatBoss <ivan.solodovnikov@outlook.com>
2025-08-01 12:27:38 +0300

    feat: implement file indexing

---
+++ src/Files/File.c
@@ -0,0 +1,198 @@
+#include "File.h"
+
+#include <assert.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "Common/List.h"
+#include "Common/Strings.h"
+
+void file_index_init(
+    FileIndex* index,
+    int override_timestamp,
+    ...
+) {
+  list_init(&index->files);
+
+  if (override_timestamp) {
+    va_list args;
+    va_start(args, override_timestamp);
+    time_t override_value = va_arg(args, time_t);
+    va_end(args);
+
+    index->override_timestamp = 1;
+    index->override_value = override_value;
+  } else {
+    index->override_timestamp = 0;
+    index->override_value = 0;
+  }
+}
+
+void file_index_clear(FileIndex* index) {
+  LinkedListNode* node = NULL;
+  while ((node = list_pop_front(&index->files))) {
+    IndexedFile* file = (IndexedFile*) node;
+
+    for (size_t i = 0; i < FILE_MAX_TAGS; ++i) {
+      free(file->tags[i]);
+    }
+
+    free(file->path);
+    free(file);
+  }
+}
+
+file_error_t file_add_to_index(FileIndex* index, const char* path) {
+  /* Check if file exists and is readable */
+  if (access(path, R_OK) != 0) {
+    if (errno == ENOENT || errno == ENOTDIR) {
+      return FERR_INVALID_PATH;
+    }
+
+    return FERR_ACCESS_DENIED;
+  }
+
+  time_t timestamp = 0;
+  if (index->override_timestamp) {
+    /* Override timestamp */
+    timestamp = index->override_value;
+  } else {
+    /* Keep original timestamp */
+    struct stat file_stat;
+    int res = stat(path, &file_stat);
+    if (res != 0) {
+      return FERR_ACCESS_DENIED;
+    }
+
+    timestamp = file_stat.st_ctime;
+  }
+
+  /* Initialize IndexedFile */
+  IndexedFile* file = (IndexedFile*) calloc(1, sizeof(*file));
+  list_node_init(&file->as_node);
+  file->path = copy_string(path);
+  file->timestamp = timestamp;
+  file->tag_count = 0;
+  for (size_t i = 0; i < FILE_MAX_TAGS; ++i) {
+    file->tags[i] = NULL;
+  }
+
+  /* Add file to index */
+  list_push_back(&index->files, &file->as_node);
+
+  return FERR_NONE;
+}
+
+static int string_compare(const void* lhs, const void* rhs) {
+  return strcmp(*(const char* const*) lhs, *(const char* const*) rhs);
+}
+
+static void get_unique_tags(
+  const IndexedFile* file,
+  const char* unique_tags[],
+  size_t* unique_count
+) {
+  const char* tags[FILE_MAX_TAGS];
+  for (size_t i = 0; i < file->tag_count; ++i) {
+    tags[i] = file->tags[i];
+  }
+  qsort(tags, file->tag_count, sizeof(tags[0]), string_compare);
+  size_t count = 0;
+  for (size_t i = 0; i < file->tag_count; ++i) {
+    if (i == 0 || strcmp(tags[i], tags[i-1]) != 0) {
+      unique_tags[count] = tags[i];
+      count++;
+    }
+  }
+  *unique_count = count;
+}
+
+static const char* get_extension(const char* path) {
+  const char* last_dot = strrchr(path, '.');
+  const char* last_slash = strrchr(path, '/');
+  int has_dot = (last_dot != NULL);
+  int has_slash = (last_slash != NULL);
+  const char* extension = "";
+  /*
+   * RANT INCOMING 
+   *
+   * This snippet would be somewhat easier to write if not for the fact that
+   * an obvious `NULL < ptr` for any non-null `ptr` isn't true under any of
+   * C standards and instead invokes undefined behavior, which means that both
+   * pointers have to be checked for equality with NULL before comparison.
+   */
+  if (has_dot && has_slash && last_dot > last_slash) {
+    extension = last_dot + 1;
+  } else if (has_dot && !has_slash) {
+    extension = last_dot + 1;
+  }
+  return extension;
+}
+
+unsigned long file_generate_name(
+  const IndexedFile* file,
+  unsigned long buf_length,
+  char name_buf[]
+) {
+  enum {
+    DATE_BUFSIZE = 11 /* YYYY-MM-DD\0 */
+  };
+  /* Format timestamp */
+  const struct tm* time = gmtime(&file->timestamp);
+  char date_buf[DATE_BUFSIZE];
+  strftime(date_buf, DATE_BUFSIZE, "%Y-%m-%d", time);
+
+  /* Get unique sorted tags */
+  const char* tags[FILE_MAX_TAGS];
+  size_t unique_count = 0;
+  get_unique_tags(file, tags, &unique_count);
+
+  /* Get file extension */
+  const char* extension = get_extension(file->path);
+
+  /* Catenate all parts of file name */
+  unsigned long total_len = 0;
+
+  name_buf[0] = '\0';
+  total_len = append_string(name_buf, buf_length, date_buf);
+
+  /* Add tags, separated by underscores */
+  for (size_t i = 0; i < unique_count; ++i) {
+    total_len = append_string(name_buf, buf_length, "_");
+    total_len = append_string(name_buf, buf_length, tags[i]);
+  }
+
+  /* Add extension if present */
+  if (extension[0] != '\0') {
+    total_len = append_string(name_buf, buf_length, ".");
+    total_len = append_string(name_buf, buf_length, extension);
+  }
+
+  return total_len;
+}
+
+file_error_t file_add_tag(IndexedFile* file, const char* tag) {
+  for (const char* ch = tag; *ch != '\0'; ++ch) {
+    int is_lower_alpha = ('a' <= *ch && *ch <= 'z');
+    int is_dash = (*ch == '-');
+
+    if (!is_lower_alpha && !is_dash) {
+      return FERR_INVALID_TAG;
+    }
+  }
+
+  if (file->tag_count == FILE_MAX_TAGS) {
+    return FERR_TOO_MANY_TAGS;
+  }
+
+  file->tags[file->tag_count] = copy_string(tag);
+  ++file->tag_count;
+
+  return FERR_NONE;
+}
---
+++ src/Files/File.h
@@ -0,0 +1,131 @@
+/**
+ * @file File.h
+ * @author MeerkatBoss (solodovnikov.ia@phystech.su)
+ *
+ * @brief Data structures for indexing and tagging files
+ *
+ * @version 0.1
+ * @date 2024-09-04
+ *
+ * @copyright Copyright MeerkatBoss (c) 2024
+ */
+#ifndef __FILES_FILE_H
+#define __FILES_FILE_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <time.h>
+
+#include "Common/List.h"
+
+/**
+ * @brief Maximum number of tags that can be attached to file
+ */
+#define FILE_MAX_TAGS 8
+
+/**
+ * @brief Success status of file operation
+ */
+enum FileError {
+  FERR_NONE,          /*!< No error */
+  FERR_INVALID_PATH,  /*!< Invalid path to file */
+  FERR_ACCESS_DENIED, /*!< Denied access to file */
+  FERR_INVALID_TAG,   /*!< Tag contains invalid characters */
+  FERR_TOO_MANY_TAGS  /*!< Number of tags exceeds FILE_MAX_TAGS */
+};
+
+typedef int file_error_t;
+
+/**
+ * @brief Description of action that should be performed on file
+ */
+enum FileAction {
+  FACT_COPY,    /*!< File should be copied to destination directory */
+  FACT_MOVE,    /*!< File should be moved to destination directory */
+  FACT_IGNORE,  /*!< File should be skipped */
+  FACT_DELETE   /*!< File should be deleted from source directory */
+};
+
+typedef int file_action_t;
+
+/**
+ * @brief Description of pending changes to file
+ */
+typedef struct {
+  file_action_t action;
+} FileChanges;
+
+/**
+ * @brief Description of file found in source directory
+ */
+typedef struct {
+  LinkedListNode as_node;
+
+  char* path;       /*!< Full path to file */
+  time_t timestamp; /*!< File creation date */
+
+  unsigned tag_count;         /*!< Number of tags added to file */
+  char* tags[FILE_MAX_TAGS];  /*!< File tags */
+
+  FileChanges changes;  /*!< Changes to be applied */
+} IndexedFile;
+
+/**
+ * @brief Description of all files found in source directory
+ */
+typedef struct {
+  LinkedList files; /*!< List of indexed files */
+
+  int override_timestamp;
+  time_t override_value;
+} FileIndex;
+
+/**
+ * @brief Initialize empty file index
+ */
+void file_index_init(
+    FileIndex* index,
+    int override_timestamp,
+    ... /* time_t override_value */
+);
+
+/**
+ * @brief Remove all files from index
+ */
+void file_index_clear(FileIndex* index);
+
+/**
+ * @brief Add file at `path` to index with given root node
+ *
+ * @return FileError status
+ */
+file_error_t file_add_to_index(
+  FileIndex* index, /*!< [inout] List of indexed files */
+  const char* path  /*!< [in]    Path to indexed file */
+);
+
+/**
+ * @brief Generate new name for file based on timestamp and tags
+ *
+ * @note
+ * File name has the following format:
+ * `YYYY-MM-DD_underscore_separated_tags.extension`
+ * Tags are sorted alphabetically and duplicate tags are removed.
+ *
+ * @return Length of generated name. If this length exceeds buffer length,
+ *         the name is truncated to fit.
+ */
+unsigned long file_generate_name(
+  const IndexedFile* file,    /*!< [in]  Target file */
+  unsigned long buf_length,   /*!< [in]  Length of `name_buf` */
+  char name_buf[]             /*!< [out] Output buffer for file name */
+);
+
+/**
+ * @brief Add tag to list of file tags. Duplicate tags are ignored.
+ *
+ * @note Tags can only contain lowercase latin letters and dash symbol '-'
+ */
+file_error_t file_add_tag(IndexedFile* file, const char* tag);
+
+#endif /* File.h */
